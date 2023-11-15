47fdec1212ced27e14c2888016cdb5cedda56af0
LemonTreeLT <108186846+LemonTreeLT@users.noreply.github.com>
2023-11-15 22:39:59 +0800

    update

--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -28,6 +28,13 @@ public class Utils {
         }
     }
 
+    /**
+     * api请求器
+     * @param urlString Api链接
+     * @param videoID 视频链接
+     * @return 视频信息
+     * @throws IOException 文件系统错误
+     */
     public JSONObject request(String urlString, String videoID) throws IOException {
         URL url = new URL(urlString + videoID);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -36,34 +43,30 @@ public class Utils {
         int responseCode = connection.getResponseCode();
         if(responseCode == 200) {
             BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String line;
             StringBuilder response = new StringBuilder();
+            String line;
 
-            while((line = reader.readLine()) != null) {
-                response.append(line);
-            }
+            while((line = reader.readLine()) != null) response.append(line);
             reader.close();
 
             String apiData = response.toString();
+            connection.disconnect();
             return JSONObject.parseObject(apiData);
-        } else {
-            System.out.println("API请求失败，响应码：" + responseCode);
         }
-
+        logger.Debug(String.valueOf(responseCode));
         connection.disconnect();
-
-        return null;
+        throw new IOException();
     }
 
     /**
-     * 警告: 下载文件的格式只能通过修改源码修改
+     * 字节流下载器
      * @param httpUrl 请求的网络链接
      * @param dir 下载目录
      * @param fileName 下载的文件名字
      * @return 成功与否
-     * @throws IOException 正常报错
+     * @throws IOException 文件系统错误
      */
-    public boolean DownlandFile(String httpUrl, String dir, String fileName) throws IOException {
+    public boolean DownlandFile(String httpUrl, String dir, String fileName, String suffix) throws IOException {
         File ImageDir = new File(dir);
         URL url = new URL(httpUrl);
         URLConnection conn = url.openConnection();
@@ -72,7 +75,7 @@ public class Utils {
         if (!ImageDir.exists()) if (!ImageDir.mkdir()) return false;
 
         InputStream inStream = conn.getInputStream();
-        try(FileOutputStream fs = new FileOutputStream((dir + "\\" + fileName + ".jpg"))) {
+        try(FileOutputStream fs = new FileOutputStream((dir + "\\" + fileName + "." + suffix))) {
             byte[] buffer = new byte[1204];
 
             while((byteRead = inStream.read(buffer)) != -1) fs.write(buffer, 0, byteRead);
@@ -89,15 +92,11 @@ public class Utils {
         System.out.println("================================================================");
     }
 
-    public String compressToFixedLength(String input) {
+    public String strToSHA256(String input) {
         try {
-            // 使用 SHA-256 哈希函数
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
-
-            // 将输入数据转换为字节数组并更新哈希对象
             byte[] hashBytes = digest.digest(input.getBytes(StandardCharsets.UTF_8));
 
-            // 将字节数组转换为十六进制字符串
             StringBuilder hexString = new StringBuilder();
             for (byte hashByte : hashBytes) {
                 String hex = Integer.toHexString(0xff & hashByte);
@@ -109,7 +108,7 @@ public class Utils {
 
             return hexString.toString();
         } catch (NoSuchAlgorithmException e) {
-            logger.Error("发生错误: " + e.getMessage());
+            logger.Error("发生错误: " + e);
             return "Error";
         }
     }
@@ -126,7 +125,7 @@ public class Utils {
     }
 
     public String getVideoInfo(String url) {
-        logger.Info("获取剪切板 | S:" + url.length() + " | " + compressToFixedLength(url));
+        logger.Info("获取剪切板 | S:" + url.length() + " | " + strToSHA256(url));
         String bvid = Search(url, Constant.StringPattern);
         if(bvid == null) return null;
         else logger.Info("解析到BvId: " + bvid);
@@ -138,6 +137,7 @@ public class Utils {
         try {
             jsonObject = request(Constant.ApiUrl, bvid);
         } catch(IOException e) {
+            logger.Error("Api请求失败,请检查你的网络链接 错误位于: " + e);
             return null;
         }
         long endTime = System.currentTimeMillis();
@@ -150,7 +150,6 @@ public class Utils {
 
         JSONObject BVData = JSONObject.parseObject(jsonObject.get("data").toString());
         JSONObject VideoStat = JSONObject.parseObject(BVData.get("stat").toString());
-
         String VideoTitle = ByteToUtf8((String) BVData.get("title"));
 
         logger.Info("已获取视频信息: " + VideoTitle);
@@ -160,10 +159,10 @@ public class Utils {
         else logger.Info("获取图片链接: " + PicUrl);
 
         try {
-            if(DownlandFile(PicUrl, Constant.DEFAULT_TEMP_FILE_DIR, bvid)) logger.Info("成功获取图片");
+            if(DownlandFile(PicUrl, Constant.DEFAULT_TEMP_FILE_DIR, bvid, "jpg")) logger.Info("成功获取图片");
             else logger.Error("未知错误,无法获取图片,请检查你的网络连接");
         } catch(Exception e) {
-            logger.Error("致命错误,位于: " + e.getMessage());
+            logger.Error("致命错误,位于: " + e);
             return null;
         }
 
